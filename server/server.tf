eb2bec740c098c45bfa83ea5b911b15c91ddc4b6
jontok <jonas@tokmaji.de>
2024-08-17 16:03:14 +0200

    fix(server-firewall): set correct var for firewall

--- server/server.tf
+++ server/server.tf
@@ -9,19 +9,19 @@ resource "hcloud_server" "server" {
   image        = "ubuntu-22.04"
   server_type  = var.server_type
   location     = var.region
-  firewall_ids = [var.network_id]
+  firewall_ids = [var.firewall_id]
   ssh_keys     = ["${hcloud_ssh_key.default.id}"]
-  
-  
-  user_data   = var.cloud_init!="" ? file(var.cloud_init) : ""
-    
-  
+
+
+  user_data = var.cloud_init != "" ? file(var.cloud_init) : ""
+
+
 
   network {
     network_id = var.network_id
     ip         = var.server_ips[count.index]
   }
-  
+
   public_net {
     ipv4_enabled = var.ipv4
     ipv6_enabled = var.ipv4
